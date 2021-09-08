574e450918b26836925fac3c60ffb46f47999467
Urodacus <muhammadalaidy@protonmail.com>
2021-09-08 03:20:13 +0200

    active selection

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -7,12 +7,28 @@ const LibrarySong = ({
   id,
   audioRef,
   isPlaying,
+  setSongs,
 }) => {
   //event handler
 
   const songSelectHandler = () => {
     const selectedSong = songs.filter((state) => state.id === id);
     setCurrentSong(selectedSong[0]);
+    // Adding active state
+    const newSongs = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
     // check if the song is playing
     if (isPlaying) {
       const playPromise = audioRef.current.play();
@@ -25,7 +41,10 @@ const LibrarySong = ({
   };
 
   return (
-    <div onClick={songSelectHandler} className="library-song">
+    <div
+      onClick={songSelectHandler}
+      className={`library-song ${song.active ? "selected" : ""}`}
+    >
       <img alt={song.name} src={song.cover}></img>
       <div className="song-description">
         <h3>{song.name}</h3>
