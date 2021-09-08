3a3d82389896cb4f15fef56c40c3a36712a4b595
Urodacus <muhammadalaidy@protonmail.com>
2021-09-08 03:03:54 +0200

    state management refactoring

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState, useRef } from "react";
 //Adding Components
 import Player from "./components/Player";
 import Song from "./components/Song";
@@ -7,16 +7,47 @@ import Library from "./components/Library";
 import data from "./utils";
 
 function App() {
+  // Ref
+  const audioRef = useRef(null);
   // state
-  const [songs,setSongs] =  useState(data())
-  const [currentSong , setCurrentSong] = useState(songs[0])
-  const [isPlaying, setIsPlaying] = useState(false)
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  // State
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+  });
 
+  // handler
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ ...songInfo, currentTime: current, duration });
+  };
   return (
     <div className="App">
-      <Song currentSong = {currentSong}/>
-      <Player setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong = {currentSong}/>
-      <Library songs={songs} setCurrentSong={setCurrentSong}/>
+      <Song currentSong={currentSong} />
+      <Player
+        audioRef={audioRef}
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+        setSongInfo={setSongInfo}
+        songInfo={songInfo}
+      />
+      <Library
+        audioRef={audioRef}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        isPlaying={isPlaying}
+      />
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
     </div>
   );
 }
