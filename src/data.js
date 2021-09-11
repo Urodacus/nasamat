3d5c7ac903693bb220d7cdb49b2ef6c60235ef3e
Urodacus <muhammadalaidy@protonmail.com>
2021-09-12 01:35:39 +0200

    data update

--- src/data.js
+++ src/data.js
@@ -2,102 +2,102 @@ import { v4 as uuidv4 } from "uuid";
 
 function audios() {
   return [
-    { 
-      head:"اقتباس الأنوار من الإحياء",
-      name: "إقتباس الأنوار من الإحياء - كتاب ذم الكبر والعجب - الدرس الأول",
+    {
+      head: "اقتباس الأنوار من الإحياء",
+      name: "إقتباس الأنوار من الإحياء - كتاب التوبة - الدرس الأول",
       cover:
         "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
       author: "Habib Omar - http://alhabibomar.com",
       audio:
-        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas104.lite.mp3",
+        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas120.lite.mp3",
       color: ["#205950, #2ab3bf"],
       id: uuidv4(),
       active: true,
     },
     {
-      name: "إقتباس الأنوار من الإحياء - كتاب ذم الكبر والعجب - الدرس الثاني",
+      name: "إقتباس الأنوار من الإحياء - كتاب التوبة - الدرس الثاني",
       cover:
         "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
       author: "Habib Omar - http://alhabibomar.com",
       audio:
-        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas105.lite.mp3",
+        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas121.lite.mp3",
       color: ["#205950, #2ab3bf"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "إقتباس الأنوار من الإحياء - كتاب ذم الكبر والعجب - الدرس الثالث",
+      name: "إقتباس الأنوار من الإحياء - كتاب التوبة - الدرس الثالث",
       cover:
         "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
       author: "Habib Omar - http://alhabibomar.com",
       audio:
-        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas106.lite.mp3",
+        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas122.lite.mp3",
       color: ["#205950, #2ab3bf"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "إقتباس الأنوار من الإحياء - كتاب ذم الكبر والعجب - الدرس الرابع",
+      name: "إقتباس الأنوار من الإحياء - كتاب التوبة - الدرس الرابع",
       cover:
         "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
       author: "Habib Omar - http://alhabibomar.com",
       audio:
-        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas107.lite.mp3",
+        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas123.lite.mp3",
       color: ["#205950, #2ab3bf"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "إقتباس الأنوار من الإحياء - كتاب ذم الكبر والعجب - الدرس الخامس",
+      name: "إقتباس الأنوار من الإحياء - كتاب التوبة - الدرس الخامس",
       cover:
         "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
       author: "Habib Omar - http://alhabibomar.com",
       audio:
-        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas108.lite.mp3",
+        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas125.lite.mp3",
       color: ["#205950, #2ab3bf"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "إقتباس الأنوار من الإحياء - كتاب ذم الكبر والعجب - الدرس السادس",
+      name: "إقتباس الأنوار من الإحياء - كتاب التوبة - الدرس السادس",
       cover:
         "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
       author: "Habib Omar - http://alhabibomar.com",
       audio:
-        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas109.lite.mp3",
+        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas126.lite.mp3",
       color: ["#205950, #2ab3bf"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "إقتباس الأنوار من الإحياء - كتاب ذم الكبر والعجب - الدرس السابع",
+      name: "إقتباس الأنوار من الإحياء - كتاب التوبة - الدرس السابع",
       cover:
         "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
       author: "Habib Omar - http://alhabibomar.com",
       audio:
-        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas110.lite.mp3",
+        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas127.lite.mp3",
       color: ["#205950, #2ab3bf"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "إقتباس الأنوار من الإحياء - كتاب ذم الكبر والعجب - الدرس الثامن",
+      name: "إقتباس الأنوار من الإحياء - كتاب عجائب القلب - الدرس الأول",
       cover:
         "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
       author: "Habib Omar - http://alhabibomar.com",
       audio:
-        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas111.lite.mp3",
+        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas1.lite.mp3",
       color: ["#205950, #2ab3bf"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "إقتباس الأنوار من الإحياء - كتاب ذم الكبر والعجب - الدرس التاسع",
+      name: "إقتباس الأنوار من الإحياء - كتاب عجائب القلب - الدرس الثاني",
       cover:
         "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
       author: "Habib Omar - http://alhabibomar.com",
       audio:
-        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas112.lite.mp3",
+        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas2.lite.mp3",
       color: ["#205950, #2ab3bf"],
       id: uuidv4(),
       active: false,
