b39810b7f75b26701eccea4f3588bc3bd93bad56
Urodacus <muhammadalaidy@protonmail.com>
2021-09-20 10:58:08 +0200

    updated data

--- src/data.js
+++ src/data.js
@@ -3,6 +3,136 @@ import { v4 as uuidv4 } from "uuid";
 function audios() {
   return [
     {
+      head: "إرشادات السلوك",
+      name: "خصائص لا إله إلا الله وكسب سعادة الدارين للمتحققين بها وشقاء الدارين لمنكريها",
+      cover:
+        "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
+      author: "Habib Omar - http://alhabibomar.com",
+      audio: " https://f002.backblazeb2.com/file/habib01/audio/m43/m100243.mp3",
+      color: ["#205950, #2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "آثار دعوة المبعوث بالحق وعجائب دوامها وانتشارها في أمته",
+      cover:
+        "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
+      author: "Habib Omar - http://alhabibomar.com",
+      audio: " https://f002.backblazeb2.com/file/habib01/audio/m43/m030243.mp3",
+      color: ["#205950, #2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "لوامع الوداد الرباني بشوارق أنوار دعوة صفوته العدناني",
+      cover:
+        "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
+      author: "Habib Omar - http://alhabibomar.com",
+      audio: " https://f002.backblazeb2.com/file/habib01/audio/m43/m260143.mp3",
+      color: ["#205950, #2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "شرف عرض الرحمن لعباده وخسة عروض المفسدين والكافرين والنتائج الكبرى لكل منها وخصوصية تجارة الأبد",
+      cover:
+        "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
+      author: "Habib Omar - http://alhabibomar.com",
+      audio: " https://f002.backblazeb2.com/file/habib01/audio/m43/m190143.mp3",
+      color: ["#205950, #2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "أسرار العناية والمحبة الربانية وتفرُّعاتها من حضرة سيد محبوبيه إلى مَن سبقت لهم السعادة",
+      cover:
+        "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
+      author: "Habib Omar - http://alhabibomar.com",
+      audio: " https://f002.backblazeb2.com/file/habib01/audio/m43/m120143.mp3",
+      color: ["#205950, #2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "عجائب معاني إتمام النعمة وكريم مجالي الرحمة",
+      cover:
+        "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
+      author: "Habib Omar - http://alhabibomar.com",
+      audio: " https://f002.backblazeb2.com/file/habib01/audio/m43/m040143.mp3",
+      color: ["#205950, #2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+
+    {
+      head: "شرح كتاب عوارف المعارف",
+      name: "عوارف المعارف - الدرس الأول - منشأ علوم الصوفية(1)",
+      cover:
+        "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
+      author: "Habib Omar - http://alhabibomar.com",
+      audio:
+        " https://f002.backblazeb2.com/file/habib01/audio/12/awaref/awaref1.lite.mp3",
+      color: ["#205950, #2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "عوارف المعارف - الدرس الثاني - منشأ علوم الصوفية(2)",
+      cover:
+        "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
+      author: "Habib Omar - http://alhabibomar.com",
+      audio:
+        " https://f002.backblazeb2.com/file/habib01/audio/12/awaref/awaref2.lite.mp3",
+      color: ["#205950, #2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "عوارف المعارف - الدرس الثالث - تخصيص الصوفية بحسن الاستماع",
+      cover:
+        "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
+      author: "Habib Omar - http://alhabibomar.com",
+      audio:
+        " https://f002.backblazeb2.com/file/habib01/audio/12/awaref/awaref3.lite.mp3",
+      color: ["#205950, #2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "عوارف المعارف - الدرس الرابع - فضيلة علوم الصوفية(1)",
+      cover:
+        "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
+      author: "Habib Omar - http://alhabibomar.com",
+      audio:
+        " https://f002.backblazeb2.com/file/habib01/audio/12/awaref/awaref4.lite.mp3",
+      color: ["#205950, #2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "عوارف المعارف - الدرس الخامس - فضيلة علوم الصوفية(2)",
+      cover:
+        "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
+      author: "Habib Omar - http://alhabibomar.com",
+      audio:
+        " https://f002.backblazeb2.com/file/habib01/audio/12/awaref/awaref5.lite.mp3",
+      color: ["#205950, #2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "عوارف المعارف - الدرس السادس - فضيلة علوم الصوفية(3)",
+      cover:
+        "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
+      author: "Habib Omar - http://alhabibomar.com",
+      audio:
+        " https://f002.backblazeb2.com/file/habib01/audio/12/awaref/awaref6.lite.mp3",
+      color: ["#205950, #2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+
+    {
       head: "اقتباس الأنوار من الإحياء",
       name: "إقتباس الأنوار من الإحياء - كتاب التوبة - الدرس الأول",
       cover:
@@ -98,6 +228,39 @@ function audios() {
       author: "Habib Omar - http://alhabibomar.com",
       audio:
         " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas2.lite.mp3",
+      color: ["#205950, #2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "إقتباس الأنوار من الإحياء - كتاب رياضة النفس وتهذيب الأخلاق - الدرس الأول",
+      cover:
+        "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
+      author: "Habib Omar - http://alhabibomar.com",
+      audio:
+        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas3.lite.mp3",
+      color: ["#205950, #2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "إقتباس الأنوار من الإحياء - كتاب رياضة النفس وتهذيب الأخلاق - الدرس الثاني",
+      cover:
+        "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
+      author: "Habib Omar - http://alhabibomar.com",
+      audio:
+        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas4.lite.mp3",
+      color: ["#205950, #2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "إقتباس الأنوار من الإحياء - كتاب رياضة النفس وتهذيب الأخلاق - الدرس الثالث",
+      cover:
+        "https://upload.wikimedia.org/wikipedia/commons/0/07/Habib_Umar_bin_Hafidz%2C_Nabawi_TV%2C_08.44.jpg",
+      author: "Habib Omar - http://alhabibomar.com",
+      audio:
+        " https://f002.backblazeb2.com/file/habib01/audio/d/egtbas/egtbas5.lite.mp3",
       color: ["#205950, #2ab3bf"],
       id: uuidv4(),
       active: false,
